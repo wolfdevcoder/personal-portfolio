855e85dab3d04474c2381956c1f46b07177c52ae
Devansh <devansh.docthub@gmail.com>
2025-04-10 17:33:09 -0400

    added analytics

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import App from "./App";
 import "./styles/global.scss";
 import "./styles/index.css";
 import { BrowserRouter } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
+      <Analytics />
       <App />
     </BrowserRouter>
   </React.StrictMode>
